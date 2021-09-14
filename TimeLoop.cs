87cb08969a6220a42e0f54b5e1b994b78a03c640
Ibaad <khanibaad@gmail.com>
2021-09-14 12:18:49 -0400

    Time loop

---
+++ TimeLoop.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleAppTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+
+            if (x >= 0 && x <= 100)
+            {
+                for (int i = 1; i <= x; i++)
+                {
+                    Console.WriteLine(i + " " + "Abracadabra");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Please Choose A Number between 1 and 100");
+            }
+
+
+
+
+        }
+    }
+}
